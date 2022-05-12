b3aa624dddef702390c954771828e2fa8a600f54
FadliGr1 <fadliblod00@gmail.com>
2022-05-12 21:38:07 +0700

    modul 7

--- LifeCycle/praktikum6/src/main/java/com/fadli_19102017/praktikum6/adapter/CardViewMyDataAdapter.kt
+++ LifeCycle/praktikum6/src/main/java/com/fadli_19102017/praktikum6/adapter/CardViewMyDataAdapter.kt
@@ -1,5 +1,7 @@
 package com.fadli_19102017.praktikum6.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,10 +12,12 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.fadli_19102017.praktikum6.DetailActivity
+import com.fadli_19102017.praktikum6.MainActivity
 import com.fadli_19102017.praktikum6.MyData
 import com.fadli_19102017.praktikum6.R
 
-class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
+class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>, val context: Context) :
     RecyclerView.Adapter<CardViewMyDataAdapter.CardViewViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewViewHolder {
         val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_cardview, parent, false)
@@ -31,8 +35,11 @@ class CardViewMyDataAdapter(private val listMyDatas: ArrayList<MyData>) :
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
         holder.btnShare.setOnClickListener { Toast.makeText(holder.itemView.context, "Share " +
                 listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
-        holder.itemView.setOnClickListener { Toast.makeText(holder.itemView.context, "Kamu memilih " +
-                listMyDatas[holder.adapterPosition].name, Toast.LENGTH_SHORT).show() }
+        holder.itemView.setOnClickListener {
+            val moveWithObjectIntent = Intent(context, DetailActivity::class.java)
+            moveWithObjectIntent.putExtra(DetailActivity.EXTRA_MYDATA, myData)
+            context.startActivity(moveWithObjectIntent)
+        }
     }
     override fun getItemCount(): Int {
         return listMyDatas.size
